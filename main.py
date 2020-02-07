e3617dd5062b3cfa2a7c851b62c411ed6439dae7
Piña <adanielpincab@gmail.com>
2020-02-07 23:26:20 +0100

    intentanco hacer que lo basico funcione

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyxel
+import math
 
 HEIGHT = 200
 WIDTH = 250
@@ -15,30 +16,41 @@ FRICTION_CONSTANT = 0.8
 # 1940 cv --> 0 a 100 en 1.85 s
 
 class Car:
-    def __init__(self, cv, mass, coords):
-       self.cv = cv
-       self.mass = mass
+    def __init__(self, coords):
        self.x = coords[0]
        self.y = coords[1]
        self.vel = 0
-       self.angle = 90
-       # self.ac_x = 0
-       # self.ac_y = 0
-       self.friction_ac = FRICTION_CONSTANT*self.mass*9.8
+       self.angle = math.pi/2
+       self.ac = 0
 
     def update(self):
         if pyxel.btn(pyxel.KEY_UP):
-            self.vel_x +=  self.vel*
+            self.ac = 5/60
+        elif pyxel.btn(pyxel.KEY_DOWN):
+            self.ac = -5/60
+        else:
+            self.ac += 1/60 * (-1 if self.vel > 0 else 1)
+        
+        if not int( self.vel ) == 0:
+            if pyxel.btn(pyxel.KEY_RIGHT):
+                self.angle -= 0.3/self.vel
+            elif pyxel.btn(pyxel.KEY_LEFT):
+                self.angle += 0.3/self.vel
+        
+        if abs( self.vel ) < 0.01:
+            self.vel = 0
 
-        self.x += self.vel_x
-        self.y += self.vel_y
+        self.vel += self.ac
+        print(self.vel)
+        self.x += 0.1*self.vel*math.cos(self.angle)
+        self.y -= 0.1*self.vel*math.sin(self.angle)
 
     def draw(self):
         pyxel.circ(self.x, self.y, 3, 7)
 
 class App:
     def __init__(self):
-        self.car = Car(60, (WIDTH/2, HEIGHT/2))
+        self.car = Car((WIDTH/2, HEIGHT/2))
 
         pyxel.init(WIDTH, HEIGHT, caption='Gran Rutismo', fps=60)
         pyxel.run(self.update, self.draw)
